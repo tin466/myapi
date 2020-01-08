dc669b423ce3ab6b0e70e9eb34e0eb1825f2c317
TinZar <tin71639@gmail.com>
2020-01-08 16:28:48 +0630

    first commit

---
+++ app/Http/Controllers/Api/Postcontroller.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use APP\Post;
+
+class Postcontroller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post= new Post;
+        $post->title=request('title');
+        $post->body=request('body');
+        $post->save();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {    
+        $request->validate([
+            'title'=>'required|min:5';
+            'body'=>'required|min:10',
+        ])
+        $post= new Post;
+        $post->title=request('title');
+        $post->body=request('body');
+        $post->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post=Post::find($id);
+        $post->delete();
+        return $post;
+    }
+}
